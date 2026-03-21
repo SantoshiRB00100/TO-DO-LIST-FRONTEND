2a67fc71e936559140846065918a3555b3b9ced0
Santoshi <srbaslingol@gmail.com>
2026-03-21 18:07:17 +0530

    fix backend url

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://todo-backend.onrender.com/api/todos";
+const BASE_URL = "https://to-do-list-backend-ghtu.onrender.com/api/todos";
 
 export const todoApi = {
   getAll: async () => {
@@ -10,26 +10,36 @@ export const todoApi = {
   create: async (formData) => {
     const res = await fetch(`${BASE_URL}/create`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
         ...formData,
         when: formData.when || undefined,
       }),
     });
-    return res.json();
+
+    const data = await res.json();
+    return data;
   },
 
   update: async (id, updates) => {
     const res = await fetch(`${BASE_URL}/update/${id}`, {
       method: "PUT",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(updates),
     });
+
     return res.json();
   },
 
   delete: async (id) => {
-    const res = await fetch(`${BASE_URL}/delete/${id}`, { method: "DELETE" });
+    const res = await fetch(`${BASE_URL}/delete/${id}`, {
+      method: "DELETE",
+    });
+
     return res.json();
   },
 };
